90de0e205141652a4c75c1310ff8deec52e64f36
Ankit Mishra <amishm766@gmail.com>
2021-10-24 15:12:19 +0530

    edited environment variables

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  API_BASE_URL: process.env?.API_BASE_URL
-    ? process.env.API_BASE_URL
-    : 'https://robofiui.herokuapp.com',
+  API_BASE_URL: 'https://robofi.herokuapp.com',
+  // API_BASE_URL: process.env?.API_BASE_URL
+  //   ? process.env.API_BASE_URL
+  //   : 'https://robofiui.herokuapp.com',
 };
