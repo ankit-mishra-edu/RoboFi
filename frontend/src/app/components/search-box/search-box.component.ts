bfd63325921aeeed632e802cd0a7cf64f3855172
Ankit Mishra <amishm766@gmail.com>
2021-10-31 22:13:36 +0530

    Added Search Box in sidebar
    Added Search box functionality in sidebar to have include search functionality.

---
+++ frontend/src/app/components/search-box/search-box.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { SearchBoxService } from '@app/@core/services/search-box/search-box.service';
+import { SpeechService } from '@app/@core/services/search-box/speech.service';
+import { Observable } from 'rxjs';
+import { share, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrls: ['./search-box.component.scss'],
+})
+export class SearchBoxComponent implements OnInit {
+  constructor(
+    private _searchBoxService: SearchBoxService,
+    private _speechService: SpeechService,
+  ) {}
+
+  ngOnInit(): void {
+    this._searchBoxService.listenClicks$
+      .pipe(
+        tap(() => this._speechService.listen()),
+        share(),
+      )
+      .subscribe();
+  }
+
+  get placeholder$(): Observable<string> {
+    return this._searchBoxService.placeholder$;
+  }
+
+  set searchBoxTypedKeywords(value: string) {
+    this._searchBoxService.searchBoxTypedKeywords = value;
+  }
+
+  get searchBoxKeywords$(): Observable<string> {
+    return this._searchBoxService.searchBoxKeywords$;
+  }
+
+  get listenClicks$(): Observable<string> {
+    return this._searchBoxService.listenClicks$;
+  }
+
+  set listenClicks(value: string) {
+    this._searchBoxService.listenClicks = value;
+  }
+}
--- frontend/src/app/components/shared.module.ts
+++ frontend/src/app/components/shared.module.ts
@@ -1,18 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SearchBoxComponent } from './search-box/search-box.component';
 import { SpinnerComponent } from './spinner/spinner.component';
 
-
-
 @NgModule({
-  declarations: [
-    SpinnerComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    SpinnerComponent
-  ]
+  declarations: [SpinnerComponent, SearchBoxComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [SpinnerComponent, SearchBoxComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
