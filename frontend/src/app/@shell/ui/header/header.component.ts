d9e4d49d88f887df8a404e91c66683543c5d1597
Ankit Mishra <amishm766@gmail.com>
2021-11-01 15:29:37 +0530

    User Profile changes.
    Send request for profile data only when the logged in user.id is not undefined.

--- frontend/src/app/@shell/ui/header/header.component.ts
+++ frontend/src/app/@shell/ui/header/header.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from '@app/pages/auth/services/auth.service';
 import { UserService } from '@app/pages/user/services/user.service';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
-import { share, switchMap } from 'rxjs/operators';
+import { filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -15,30 +15,31 @@ import { share, switchMap } from 'rxjs/operators';
 })
 export class HeaderComponent implements OnInit {
   constructor(
-    private router: Router,
+    private _router: Router,
     private _authService: AuthService,
     private _userService: UserService,
   ) {}
 
   path = ROUTER_UTILS.config;
   media_url = environment.MEDIA_BASE_URL;
-  isLoggedIn$!: Observable<boolean>;
 
-  userProfile$ = this._authService.loggedInUser$.pipe(
-    switchMap((loggedInUser: IUser) =>
-      this._userService.profileById$(loggedInUser.id),
-    ),
-    share(),
-  );
+  isLoggedIn$!: Observable<boolean>;
+  userProfile$!: Observable<IProfile>;
 
   ngOnInit(): void {
     this.isLoggedIn$ = this._authService.isLoggedIn$;
+    this.userProfile$ = this._authService.loggedInUser$.pipe(
+      filter((loggedInUser: IUser) => loggedInUser.id !== undefined),
+      switchMap((loggedInUser: IUser) =>
+        this._userService.profileById$(loggedInUser.id),
+      ),
+    );
   }
 
   onClickSignOut(): void {
     this._authService.signOut().subscribe(() => {
       const { root, signIn } = ROUTER_UTILS.config.auth;
-      this.router.navigate(['/', root, signIn]);
+      this._router.navigate(['/', root, signIn]);
     });
   }
 }
