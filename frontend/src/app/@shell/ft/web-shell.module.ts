300d58f7956d7b467888fcfe17125c2fbe69505d
Ankit Mishra <amishm766@gmail.com>
2021-11-18 20:18:15 +0530

    ESM lazy loaded module structure

--- frontend/src/app/@shell/ft/web-shell.module.ts
+++ frontend/src/app/@shell/ft/web-shell.module.ts
@@ -12,43 +12,86 @@ import { NotFoundPage } from '../ui/not-found/not-found.page';
 const APP_ROUTES: Routes = [
   {
     path: ROUTER_UTILS.config.base.home,
-    loadChildren: async () =>
-      (await import('@pages/home/home.module')).HomeModule,
+    loadChildren: () =>
+      import('@pages/home/home.module').then((m) => m.HomeModule),
   },
   {
     path: ROUTER_UTILS.config.auth.root,
-    loadChildren: async () =>
-      (await import('@pages/auth/auth.module')).AuthModule,
     canLoad: [NoAuthGuard],
+    loadChildren: async () =>
+      import('@pages/auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: ROUTER_UTILS.config.settings.root,
-    loadChildren: async () =>
-      (await import('@pages/settings/settings.module')).SettingsModule,
     canLoad: [AuthGuard],
     canActivate: [AuthGuard],
+    loadChildren: async () =>
+      import('@pages/settings/settings.module').then((m) => m.SettingsModule),
   },
   {
     path: ROUTER_UTILS.config.user.root,
-    loadChildren: async () =>
-      (await import('@pages/user/user.module')).UserModule,
     canLoad: [AuthGuard],
     canActivate: [AuthGuard],
+    loadChildren: async () =>
+      import('@pages/user/user.module').then((m) => m.UserModule),
   },
   {
     path: ROUTER_UTILS.config.workflow.root,
-    loadChildren: async () =>
-      (await import('@pages/workflow/workflow.module')).WorkflowModule,
     canLoad: [AuthGuard],
     canActivate: [AuthGuard],
+    loadChildren: async () =>
+      import('@pages/workflow/workflow.module').then((m) => m.WorkflowModule),
   },
   {
     path: '**',
     loadChildren: async () =>
-      (await import('@shell/ui/not-found/not-found.module')).NotFoundModule,
+      import('@shell/ui/not-found/not-found.module').then(
+        (m) => m.NotFoundModule,
+      ),
     component: NotFoundPage,
   },
 ];
+
+// const APP_ROUTES: Routes = [
+//   {
+//     path: ROUTER_UTILS.config.base.home,
+//     loadChildren: async () =>
+//       (await import('@pages/home/home.module')).HomeModule,
+//   },
+//   {
+//     path: ROUTER_UTILS.config.auth.root,
+//     loadChildren: async () =>
+//       (await import('@pages/auth/auth.module')).AuthModule,
+//     canLoad: [NoAuthGuard],
+//   },
+//   {
+//     path: ROUTER_UTILS.config.settings.root,
+//     loadChildren: async () =>
+//       (await import('@pages/settings/settings.module')).SettingsModule,
+//     canLoad: [AuthGuard],
+//     canActivate: [AuthGuard],
+//   },
+//   {
+//     path: ROUTER_UTILS.config.user.root,
+//     loadChildren: async () =>
+//       (await import('@pages/user/user.module')).UserModule,
+//     canLoad: [AuthGuard],
+//     canActivate: [AuthGuard],
+//   },
+//   {
+//     path: ROUTER_UTILS.config.workflow.root,
+//     loadChildren: async () =>
+//       (await import('@pages/workflow/workflow.module')).WorkflowModule,
+//     canLoad: [AuthGuard],
+//     canActivate: [AuthGuard],
+//   },
+//   {
+//     path: '**',
+//     loadChildren: async () =>
+//       (await import('@shell/ui/not-found/not-found.module')).NotFoundModule,
+//     component: NotFoundPage,
+//   },
+// ];
 
 @NgModule({
   imports: [
