1f570bae71a9f5b942cec0f6cce9109f711b18ca
Ankit Mishra <amishm766@gmail.com>
2021-12-18 16:15:10 +0530

    fixes for upgrading Django version to 4.0

---
+++ backend/robofi/config.py
@@ -0,0 +1,88 @@
+import os
+from datetime import timedelta
+from pathlib import Path
+
+import dj_database_url
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Setting Environment variables
+
+if (os.path.exists(os.path.join(Path(__file__).resolve().parent, 'environment.py'))):
+    from .environment import environment
+else:
+    environment = os.environ
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_CREDENTIALS = True
+
+DEBUG = environment.get('DEBUG')
+
+SECRET_KEY = environment.get('SECRET_KEY')
+
+ALLOWED_HOSTS = [environment.get('ALLOWED_HOSTS')]
+CORS_ORIGIN_WHITELIST = environment.get('ALLOWED_ORIGINS').split(',')
+
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type("application/javascript", ".js", True)
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        environment.get('DATABASE_URL'), conn_max_age=600)
+}
+
+SPLITTED_CLOUDINARY_URL = environment.get('CLOUDINARY_URL').split(':')
+
+CLOUDINARY = {
+    'cloud_name': SPLITTED_CLOUDINARY_URL[2].split('@')[1],
+    'api_key': SPLITTED_CLOUDINARY_URL[1][2:],
+    'api_secret': SPLITTED_CLOUDINARY_URL[2].split('@')[0],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=int(environment.get('ACCESS_TOKEN_EXPIRATION_TIME'))),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=int(environment.get('REFRESH_TOKEN_EXPIRATION_TIME'))),
+    'UPDATE_LAST_LOGIN': True,
+}
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = environment.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = environment.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+# Automation Variables
+
+AUTOMATION_REPO = environment.get("AUTOMATION_REPO")
+AUTOMATION_TOKEN = environment.get("AUTOMATION_TOKEN")
+AUTOMATION_README_FILE_NAME = environment.get("AUTOMATION_DETAILS_FILE_NAME")
+AUTOMATION_DETAILS_FILE_NAME = environment.get("AUTOMATION_DETAILS_FILE_NAME")
--- backend/robofi/urls.py
+++ backend/robofi/urls.py
@@ -1,7 +1,7 @@
 """robofi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
@@ -24,5 +23,6 @@ urlpatterns = [
     path('api/users/', include('pages.users.urls'), name='users-api'),
     path('api/authentication/', include('pages.authentication.urls'), name='auth-api'),
     path('api/automation/', include('pages.automation.urls'), name='automation-api'),
-    url(r'^.*', TemplateView.as_view(template_name="frontend.html"), name="frontend"),
+    re_path(r'^.*', TemplateView.as_view(template_name="frontend.html"),
+            name="frontend"),
 ]
