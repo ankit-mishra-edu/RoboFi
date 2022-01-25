d47505e6d1bebb85d55d24c1b4ae2ecd2722b40e
Ankit Mishra <amishm766@gmail.com>
2022-01-25 15:58:06 +0530

    header changes

--- frontend/src/app/@shell/ui/header/header.module.ts
+++ frontend/src/app/@shell/ui/header/header.module.ts
@@ -2,8 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '@app/components/shared.module';
-import { NavbarModule } from '../navbar/navbar.module';
 import { HeaderComponent } from './header.component';
+import { NavbarModule } from './navbar/navbar.module';
 
 @NgModule({
   declarations: [HeaderComponent],
