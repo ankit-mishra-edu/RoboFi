34b1a569ec8c8629e25cb8083d29eaf163247e6f
Ankit Mishra <amishm766@gmail.com>
2022-01-27 14:09:10 +0530

    Create session when user send request for login
    use django.login to log use in to create a session of that user in DB.

--- backend/api/authentication/sign_in/views.py
+++ backend/api/authentication/sign_in/views.py
@@ -1,7 +1,23 @@
-from .serializers import SignInSerializer
-
+from django.contrib.auth import login
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from ...users.models import User
+from .serializers import SignInSerializer
 
 
 class SignInView(TokenObtainPairView):
     serializer_class = SignInSerializer
+
+    def post(self, request, *args, **kwargs):
+        print(request.__dict__)
+        returnValue: Response = super().post(request, *args, **kwargs)
+        print(request.__dict__)
+        print(returnValue.__dict__)
+        try:
+            user = User.objects.get(username=request.data['username'])
+        except User.DoesNotExist:
+            return Response(status=404)
+
+        login(request=request, user=user)
+        return returnValue
