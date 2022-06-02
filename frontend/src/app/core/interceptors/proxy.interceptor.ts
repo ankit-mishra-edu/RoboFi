7e9f4e5c1c1fbb40fce68742f460d1feee2377ca
Ankit Mishra <ankit.mishra.edu@gmail.com>
2022-06-02 17:13:31 +0530

    Update proxy.interceptor.ts

--- frontend/src/app/core/interceptors/proxy.interceptor.ts
+++ frontend/src/app/core/interceptors/proxy.interceptor.ts
@@ -1,6 +1,5 @@
 import {
   HttpEvent,
-  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
@@ -8,7 +7,7 @@ import {
 import { Injectable } from '@angular/core';
 import { HttpLoaderStore } from '@components/http-loader/http-loader.store';
 import { environment } from '@environments/environment';
-import { finalize, Observable, tap } from 'rxjs';
+import { finalize, Observable } from 'rxjs';
 import { ENDPOINT_UTILS } from '../utils/endpoints.utils';
 
 @Injectable()
@@ -20,7 +19,7 @@ export class ProxyInterceptor implements HttpInterceptor {
     next: HttpHandler,
   ): Observable<HttpEvent<unknown>> {
     if (request.url.startsWith(`/${ENDPOINT_UTILS.config.base.home}`)) {
-      if (['POST', 'PUT', 'PATCH'].includes(request.method)) {
+      if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(request.method)) {
         this._httpLoaderStore.waitingForResponse = true;
       }
       request = request.clone({
